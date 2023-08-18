193d546e948c78c992124af33a8884aacb36f181
Fagner86 <fagnersilva009@gmail.com>
2023-08-18 18:17:22 -0300

    conectando a api-server

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   const handleConvert = async () => {
     console.log(code);
     try {
-      const response = await axios.post('http://localhost:5000/converter', { code });
+      const response = await axios.post('https://api-conversor-codigo.onrender.com', { code });
       console.log('Response:', response.data); // Verifique se a resposta está correta
       setConvertedCode(response.data.convertedCode);
     } catch (error) {
