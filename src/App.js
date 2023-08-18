0b33ae213631ab1bec13082af829522621871a28
Fagner86 <fagnersilva009@gmail.com>
2023-08-18 13:51:35 -0300

    removendo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const [code, setCode] = useState('');
+  const [convertedCode, setConvertedCode] = useState('');
+
+  const handleConvert = async () => {
+    console.log(code);
+    try {
+      const response = await axios.post('http://localhost:5000/converter', { code });
+      console.log('Response:', response.data); // Verifique se a resposta está correta
+      setConvertedCode(response.data.convertedCode);
+    } catch (error) {
+      console.error('Erro na requisição:', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex align-items-center justify-content-center vh-100 bg-dark text-white">
+      <div className="container">
+        <h1 className="text-center mb-4">Conversor de Código</h1>
+        <div className="mb-3">
+          <label htmlFor="codigo" className="form-label">Insira o Código:</label>
+          <textarea
+            id="codigo"
+            className="form-control bg-black text-white"
+            rows="10"
+            value={code}
+            onChange={(e) => setCode(e.target.value)}
+          />
+        </div>
+        <button className="btn btn-primary mb-3" onClick={handleConvert}>Converter</button>
+        <div>
+          <label htmlFor="codigoConvertido" className="form-label">Código Convertido:</label>
+          <textarea
+            id="codigoConvertido"
+            className="form-control bg-black text-white"
+            rows="10"
+            readOnly
+            value={convertedCode}
+          />
+        </div>
+      </div>
     </div>
   );
 }
